53ff63fc724ac810761f45fd026d0b6a20ec6554
agataaga <agatazaparucha@gmail.com>
2024-04-13 12:37:51 +0200

    code cleanup

--- lib/main.dart
+++ lib/main.dart
@@ -1,13 +1,13 @@
 import 'package:flutter/material.dart';
 import 'package:world_clock/pages/openBoards.dart';
-import 'package:world_clock/pages/loading.dart';
+
 import 'package:world_clock/pages/home.dart';
 import 'chessRelated/chessboardPage.dart';
 
 void main() => runApp(MaterialApp(
   initialRoute: '/home',
   routes: {
-    '/': (context) => Loading(),
+    '/': (context) => Home(),
     '/home': (context) => Home(),
     '/boards': (context) => BoardView(),
   }

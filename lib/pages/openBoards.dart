68e9a5988023d18041093a7f78c434fdb5374fdb
agataaga <agatazaparucha@gmail.com>
2024-05-07 14:10:04 +0200

    dziala!

--- lib/pages/openBoards.dart
+++ lib/pages/openBoards.dart
@@ -14,50 +14,29 @@ class BoardView extends StatefulWidget {
 }
 
 class _BoardViewState extends State<BoardView> {
-  final Future<SharedPreferences> _prefs = SharedPreferences.getInstance();
-  late Future<bool> _isDone;
-
+  late bool isDone;
   bool _isChecked = false;
-  //bool isDone = false;
   String? fenCode;
 
-  Future<void> _changeValue() async {
-    final SharedPreferences prefs = await _prefs;
-    //final bool? isDone = (prefs.getBool('isDone'));
-    bool? c = prefs.getBool('isDone');
-    final bool? isDone;
-    if (c!= null){
-      isDone= !c;
-      print("isDone: ");
-      print(isDone);
-    }
-    else{isDone = false;}
-
-
+  void loadData() async{
+    final SharedPreferences prefs = await SharedPreferences.getInstance();
 
     setState(() {
-      _isDone = prefs.setBool('isDone', isDone!).then((bool success) {
-        print("set state val");
-        print(_isDone);
-        return _isDone;
-      });
+      isDone = prefs.getBool("${fenCode}") ?? false;
     });
   }
 
-
   @override
   void initState() {
+    loadData();
     super.initState();
-    _isDone = _prefs.then((SharedPreferences prefs){
-      return prefs.getBool('isDone')?? false;
-    });
   }
 
   @override
   Widget build(BuildContext context) {
     final BoardSetup? chessData = ModalRoute.of(context)!.settings.arguments as BoardSetup?;
     if (chessData != null){
-    //isDone = chessData.isDone ?? false;
+
     fenCode = chessData.fenCode;
     }
 
@@ -71,45 +50,12 @@ class _BoardViewState extends State<BoardView> {
             centerTitle: true,
             elevation: 0,
           ),
-          floatingActionButton: FloatingActionButton(
-              onPressed: _changeValue,
-              tooltip: 'Is the task done?',
-              child: const Icon(
-                  Icons.check_box_outline_blank
-              )
-          ),
+
 
 
           body: Center(
               child: Column(
                 children: [
-                  FutureBuilder<bool>(
-                    future: _isDone,
-                    builder: (BuildContext context, AsyncSnapshot<bool> snapshot){
-                      switch(snapshot.connectionState){
-                        case ConnectionState.none:
-                        case ConnectionState.waiting:
-                          return const CircularProgressIndicator();
-                        case ConnectionState.active:
-                        case ConnectionState.done:
-                          if (snapshot.hasError){
-                      return Text ('Error: ${snapshot.error}');
-                      }
-                          else {
-                            print("wyniki:");
-                            print(snapshot.data);
-                            return Text(
-                                "the task i ${snapshot.data}"
-                            );
-                              //print(snapshot.data);
-
-                          }
-
-                      }
-                    },
-
-                  ),
-
                   SizedBox(height:30),
                   Text(
                     'FEN Code:',
@@ -139,19 +85,19 @@ class _BoardViewState extends State<BoardView> {
                     ),
                   ),
 
-                  // CheckboxListTile(
-                  //   title: Text(
-                  //       "Is the task done?",
-                  //     style: TextStyle(
-                  //       color: Colors.deepPurple,
-                  //       fontSize: 20,
-                  //     )
-                  //   ),
-                  //   value: false,
-                  //   onChanged: (newValue) async {
-                  //     _changeValue(newValue!);
-                  //   },
-                  // ),
+                  CheckboxListTile(
+                    title: Text(
+                        "Is the task done?",
+                      style: TextStyle(
+                        color: Colors.deepPurple,
+                        fontSize: 20,
+                      )
+                    ),
+                    value: isDone,
+                    onChanged: (newValue) async {
+                     updateData();
+                    },
+                  ),
                 ],
               )
           )
@@ -167,4 +113,13 @@ class _BoardViewState extends State<BoardView> {
       );
     }
   }
+  void updateData() async {
+    final SharedPreferences prefs = await SharedPreferences.getInstance();
+    setState(() {
+      isDone = !isDone;
+      prefs.setBool("${fenCode}", isDone);
+  });
+  }
 }
+
+
